09ed059d780ecd2fe68f6e0b671b75d15c0b0bd7
gadhvirushiraj <gadhvirushiraj@gmail.com>
2022-12-19 02:38:03 +0530

    made all the detectors work

--- sequential_SG.py
+++ sequential_SG.py
@@ -1,5 +1,5 @@
 
-from qutip import Qobj,Bloch, ket
+from qutip import ket
 import numpy as np 
 import matplotlib.pyplot as plt
 from collections import namedtuple
@@ -19,14 +19,19 @@ class detector_series():
             exit()
 
         data = [self.generate_random_direction() for _ in range(self.n)]
+        last_detector = (0,0)
         for i in range(len(self.series_analyser)):
             up, down = self.check_detector_type(i)
+
+            if last_detector != (up, down):
+                data = [self.generate_random_direction() for _ in range(len(data))]
 
             temp = []
             result = self.make_detector(data, up, down)[self.series_path[i]]
             temp = [data[j] for j in range(len(result)) if result[j] == True]
             
             data = temp
+            last_detector = (up, down)
             
 
     def check_detector_type(self, i):
@@ -41,8 +46,8 @@ class detector_series():
             exit()
 
     def make_detector(self, data, up, down):
-        atoms = [self.convert_to_qubit(d,up,down) for d in data]
-        spins = np.array([self.measure_zspin(q,up) for q in atoms])
+        atoms = [self.convert_to_qubit(d) for d in data]
+        spins = np.array([self.measure_spin(q,up) for q in atoms])
         self.plot_outcomes(spins)
         return [spins == 1, spins == -1]
 
@@ -56,12 +61,13 @@ class detector_series():
 
         return self.direction(theta=theta, phi=phi)
 
-    def convert_to_qubit(self, d, up, down):
+    def convert_to_qubit(self, d):
+        up, down = ket("0"), ket("1")
         return np.cos(d.theta / 2) * up + np.exp(1j * d.phi) * np.sin(d.theta / 2) * down
 
-    def measure_zspin(self, qubit, up):
-        zspin = (up.dag() * qubit).tr()
-        prob = np.abs(zspin) ** 2
+    def measure_spin(self, qubit, up):
+        spin = (up.dag() * qubit).tr()
+        prob = np.abs(spin) ** 2
         
         if 0.5 <= prob:
             return 1
@@ -71,12 +77,12 @@ class detector_series():
     def plot_outcomes(self, spins):
         fig, ax = plt.subplots()
         ax.hist(spins)
-        ax.set_xlabel("Z-component of spin")
+        ax.set_xlabel("component of spin")
         ax.set_ylabel("# of atoms")
         plt.show()
 
 
-analyser = ['Z', 'X']
-path = [0, 0]
-d1 = detector_series(analyser, path, 100)
+analyser = ['Z', 'Z', 'X', 'Z']
+path = [1, 0, 0, 0]
+d1 = detector_series(analyser, path, 1000)
 d1.sequence_data_parcel()
